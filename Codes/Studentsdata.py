8755a32c725b881933961b73139a54c7be85d74e
Joji Johnson <jojijohnsonaj@gmail.com>
2019-03-13 12:00:02 +0530

    Update Studentsdata.py

--- Codes/Studentsdata.py
+++ Codes/Studentsdata.py
@@ -1,6 +1,6 @@
 from Classes_and_objects import Student
 
-Student1 = Student ("Joji", "ECE", 7.02, True)
+Student1 = Student ("Jim", "ECE", 7.02, True)
 Student2 = Student ("Mike", "EEE", 6.99, False)
 
 print (Student1.name)
