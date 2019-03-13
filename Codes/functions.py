284e7a9fb38ad8020e02ec9b0a12a2e1091bec1e
Joji Johnson <jojijohnsonaj@gmail.com>
2019-03-13 11:53:18 +0530

    Update functions.py

--- Codes/functions.py
+++ Codes/functions.py
@@ -1,6 +1,6 @@
 def sayhi(name, age):
     print("Hello "+ name + ",you are " + str(age)) # age is converted to string.
 
-sayhi("Joji", 24) #function calling
+sayhi("Jim", 24) #function calling
 sayhi("Steve",28)
 
