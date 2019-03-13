be6e440a5b81d8523c7499cbc8bca562faa190dd
Joji Johnson <jojijohnsonaj@gmail.com>
2019-03-13 11:58:15 +0530

    Update string functions.py

--- Codes/string functions.py
+++ Codes/string functions.py
@@ -1,17 +1,17 @@
-charecter_name = "Joji"
+charecter_name = "John"
 charecter_no = "2"
 print("My name is " + charecter_name +".")
 print("I am an Engineer.")
 print("Thank you Lord for being so.")
 print("I have "+ charecter_no + " Vehicles")
-name = "Joji Johnson"
+name = "Steve"
 print(name.upper())
 print(name.upper().isupper())
 print("Number of charecters in name is", len(name))
 print(name[2])
-print(name.index("i"))
-print(name.index("Joh"))
-print(name.replace("Johnson","Joji"))
+print(name.index("v"))
+print(name.index("Ste"))
+print(name.replace("Steve","Jim"))
 
 
 
