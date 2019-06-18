0107cbf3b85cd73c57fa6474a5febb91cdae216c
Joji Johnson <jojijohnsonaj@gmail.com>
2019-06-18 14:28:28 +0530

    Update functions.py

--- Codes/functions.py
+++ Codes/functions.py
@@ -2,5 +2,5 @@ def sayhi(name, age):
     print("Hello "+ name + ",you are " + str(age)) # age is converted to string.
 
 sayhi("Jim", 24) #function calling
-sayhi("Steve",28)
+sayhi("Steve",28) #function calling
 
